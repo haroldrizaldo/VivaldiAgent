f2df2b71cdaf438742647f4885e970587178c4f4
haroldrizaldo <haroldrizaldo@gmail.com>
2017-07-07 14:16:12 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const sql = require('mssql')
 
 const restService = express();
 
@@ -11,7 +12,7 @@ restService.use(bodyParser.urlencoded({
 
 restService.use(bodyParser.json());
 
-restService.post('/echo', function(req, res) {
+restService.post('/echo', function (req, res) {
     var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Oopps! Seems like some problem. Speak again."
     return res.json({
         speech: speech,
@@ -20,91 +21,119 @@ restService.post('/echo', function(req, res) {
     });
 });
 
-restService.post('/slack-test', function(req, res) {
+restService.post('/slack-test', function (req, res) {
+    //For padding
+    var zpad = require('zpad');
 
-    var slack_message = {
-        "text": "Details of JIRA board for Browse and Commerce",
-        "attachments": [{
-            "title": "JIRA Board",
-            "title_link": "http://www.google.com",
-            "color": "#36a64f",
+    //Get the parameter values
+    //var param1 = zpad(req.body.result.parameters.floor, 2);
+    var param1 = zpad('12', 2);
+    var res1 = {};
 
-            "fields": [{
-                "title": "Epic Count",
-                "value": "50",
-                "short": "false"
-            }, {
-                "title": "Story Count",
-                "value": "40",
-                "short": "false"
-            }],
 
-            "thumb_url": "https://stiltsoft.com/blog/wp-content/uploads/2016/01/5.jira_.png"
-        }, {
-            "title": "Story status count",
-            "title_link": "http://www.google.com",
-            "color": "#f49e42",
-
-            "fields": [{
-                "title": "Not started",
-                "value": "50",
-                "short": "false"
-            }, {
-                "title": "Development",
-                "value": "40",
-                "short": "false"
-            }, {
-                "title": "Development",
-                "value": "40",
-                "short": "false"
-            }, {
-                "title": "Development",
-                "value": "40",
-                "short": "false"
-            }]
-        }]
-    }
-    return res.json({
-      speech: "List of Units",
-      messages: [
-        {
-          "type": "list_card",
-          "platform": "google",
-          "title": "Available Units",
-          "items": [
-            {
-              "optionInfo": {
-                "key": "VRC1RE36002",
-                "synonyms": []
-              },
-              "title": "[PRJ VRC] [BLDG 1] [TYPE RE] [FLR 36] [UNIT 002]",
-              "image": {
-                "url": "http://cdn4.zipmatch.com/blog/wp-content/uploads/2015/07/bi-level-condo.jpg"
-              }
-            },
-            {
-              "optionInfo": {
-                "key": "VRC1RE36003",
-                "synonyms": []
-              },
-              "title": "[PRJ VRC] [BLDG 1] [TYPE RE] [FLR 36] [UNIT 003]",
-              "image": {
-                "url": "http://cdn.decoist.com/wp-content/uploads/2014/05/Cool-living-room-with-wall-mounted-entertainment-unit.jpg"
-              }
-            }
-          ]
-        },
-        {
-          "type": 0,
-          "speech": ""
+    //SQL connection
+    const config = {
+        user: 'eurotowers',
+        password: 'd@rnasour123',
+        server: '52.187.78.66',
+        port: "1994",
+        database: 'EREMS_PORTAL_TEST',
+        options: {
+            encrypt: true // Use this if you're on Windows Azure 
         }
-      ]
+    }
+
+    // connect to your database
+    sql.connect(config, function (err) {
+
+        if (err) console.log(err);
+
+        // create Request object
+        var request = new sql.Request();
+
+        // query to the database and get the records
+
+        request.input('floorID', sql.VarChar(2), param1);
+        request.query('select top 5 ch_unit_id as [key],\'[]\' as [synonyms], rtrim(ch_unit_desc) as ch_unit_desc,mo_lp_with_cwtdisc1=CONVERT(varchar, CAST(mo_lp_with_cwtdisc AS money), 1) From mf_unit_status_details where ch_floor_id=@floorID', function (err, recordset) {
+            if (err) console.log(err)
+
+            var itemObject = {}
+
+            var len = recordset.recordset.length;
+
+            res1 = {
+                speech: "List of Units",
+                messages: [
+                    {
+                        "type": "list_card",
+                        "platform": "google",
+                        "title": "Available Units",
+                        "items": [
+                        ]
+                    },
+                    {
+                        "type": 0,
+                        "speech": ""
+                    }
+                ]
+            };
+
+            for (var i = 0; i < len; i++) {
+                itemObject = {
+                    "optionInfo": {
+                        "key": recordset.recordset[i].key,
+                        "synonyms": []
+                    },
+                    "title": recordset.recordset[i].ch_unit_desc,
+                    "image": {
+                        "url": "http://cdn4.zipmatch.com/blog/wp-content/uploads/2015/07/bi-level-condo.jpg"
+                    }
+                };
+                res1["messages"][0].items[i] = itemObject;
+            }
+
+        });
     });
+
+    return res.json(JSON.stringify(res1));
+    //return res.json({
+    //    speech: "List of Units",
+    //    messages: [
+    //        {
+    //            "type": "list_card",
+    //            "platform": "google",
+    //            "title": "Available Units",
+    //            "items": [
+    //               {
+    //                    "optionInfo": {
+    //                        "key": "VRC1RE36002",
+    //                        "synonyms": []
+    //                    },
+    //                    "title": "[PRJ VRC] [BLDG 1] [TYPE RE] [FLR 36] [UNIT 002]",
+    //                    "image": {
+    //                        "url": "http://cdn4.zipmatch.com/blog/wp-content/uploads/2015/07/bi-level-condo.jpg"
+    //                    }
+    //                },
+    //                {
+    //                    "optionInfo": {
+    //                        "key": "VRC1RE36003",
+    //                        "synonyms": []
+    //                    },
+    //                    "title": "[PRJ VRC] [BLDG 1] [TYPE RE] [FLR 36] [UNIT 003]",
+    //                    "image": {
+    //                        "url": "http://cdn.decoist.com/wp-content/uploads/2014/05/Cool-living-room-with-wall-mounted-entertainment-unit.jpg"
+    //                    }
+    //                }
+    //            ]
+    //        },
+    //        {
+    //            "type": 0,
+    //            "speech": ""
+    //        }
+    //    ]
+    //});
 });
 
-
-
-
-restService.listen((process.env.PORT || 8000), function() {
+restService.listen((process.env.PORT || 8000), function () {
     console.log("Server up and listening");
 });
